047456fca14f7456db76ae2c34027f86b8c96b03
Callie Nipper <callie.nipper@gmail.com>
2021-04-29 16:24:51 -0700

    Created route to get a single blog post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -34,3 +34,40 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// Gets one single post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'content', 'title', 'created_at'],
+
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+
+        {
+            model: Comment,
+            attributes: ['id', 'comments', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'This post cannot be found' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
