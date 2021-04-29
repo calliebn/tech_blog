c6cbdbadd0ed5f1982ff3dc97368f59fce00986c
Callie Nipper <callie.nipper@gmail.com>
2021-04-28 19:00:43 -0700

    Started post routes in api folder. Created the Get all posts route

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Gets existing posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        order: [
+            ['created_at', 'DESC']
+        ],
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData.reverse()))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
