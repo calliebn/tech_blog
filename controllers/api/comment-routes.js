2fb8325361f9f248cc3520321e0e5bc17ff5b7f4
Callie Nipper <callie.nipper@gmail.com>
2021-04-30 15:08:32 -0700

    Created comment routes in API folder

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Gets all comments
+router.get('/', withAuth, (req, res) => {
+    Comment.findAll({})
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// Gets one comment by id
+router.get('/:id', (req, res) => {
+    Comment.findAll({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// Creates a new comment
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.body.user_id,
+        })
+        res.status(200).json(newComment)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Updates an existing comment
+router.put('/:id', withAuth, (req, res) => {
+    Comment.update({
+        comment_text: req.body.comment_text
+    },
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'A comment with this id does not exist' });
+                return
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Deletes a comment
+router.delete('/id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'A comment with this id does not exist' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
