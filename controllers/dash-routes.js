c85f323e61ce01991ef573223b8d01a148f677ae
Callie Nipper <callie.nipper@gmail.com>
2021-05-01 08:57:37 -0700

    Created get new posts route for dashboard

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -88,3 +88,11 @@ router.get('/edit/:id', withAuth, (req, res) => {
             res.status(500).json(err);
         })
 });
+
+
+// Gets new posts
+router.get('/new', (req, res) => {
+    res.render('new-post');
+});
+
+module.exports = router;
