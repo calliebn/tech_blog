ff54d1acda28e2ef788add5e9c20f93cf5954352
Callie Nipper <callie.nipper@gmail.com>
2021-05-01 16:11:57 -0700

    Created comment.js file in public/js folder

---
+++ public/js/comment.js
@@ -0,0 +1,31 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+
+    const commentText = document.querySelector('#comment-body').value.trim();
+
+    const postId = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (commentText) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ postId, commentText }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+            document.querySelector('#comment-form').style.display = "block";
+        }
+    }
+};
+
+document
+    .querySelector('.comment-form')
+    .addEventListener('submit', commentFormHandler);
